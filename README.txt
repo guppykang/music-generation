65f3d43a2814428a15a102701abed181c9adebc4
jbk001 <guppykang@gmail.com>
2019-11-23 21:53:08 -0800

    finished music generation and added vanilla RNN implementation

---
+++ README.txt
@@ -0,0 +1,7 @@
+0.7 temperature seems to work the best for music generation. Generate music by converting to legitmate musical notation at : http://mandolintab.net/abcconverter.php 
+
+LSTM Model with 100 hidden units and 1 hidden layer seems to converge around 1.5 - 1.6. 
+Rnn model with 150 hidden units and 1 hidden layer seems to converge around 1.9 - 2.0
+
+RNN implementation included in addition to the LSTM. RNN is only slightly worse than the LSTM (expected). 
+
--- model.py
+++ model.py
@@ -14,11 +14,10 @@ from torchvision import transforms, utils, models
 import os
 
 
-#TODO : can add dropout in the lstm
 class Lstm(nn.Module):
     def __init__(self, input_dim, num_neurons, num_layers):
         super(Lstm, self).__init__()
-        self.lstm_layer = nn.LSTM(input_dim, num_neurons, num_layers, dropout=0)
+        self.lstm_layer = nn.LSTM(input_dim, num_neurons, num_layers, dropout=0.2)
         self.fc = nn.Linear(num_neurons, input_dim)
     
     def forward(self, x, hidden):
@@ -31,5 +30,30 @@ class Lstm(nn.Module):
             return x[None , :], hidden
         
         return x, hidden
+    
+    
 
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_dim, n_layers):
+        super(RNN, self).__init__()
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, dropout=0.2)   
+        self.fc = nn.Linear(hidden_dim, input_size)
+    
+    def forward(self, x, hidden):
+        is_size_one = (x.shape[0] == 1)
+
+        x, hidden = self.rnn(x, hidden)
+        
+#         out = out.contiguous().view(-1, self.hidden_dim)
+
+        x = self.fc(x.squeeze())
+        
+        if is_size_one:
+            return x[None , :], hidden
+        
+        return x, hidden
+    
+   
 
